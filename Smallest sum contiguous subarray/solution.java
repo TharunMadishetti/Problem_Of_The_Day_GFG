33ded7beb8a96bf10baecfd5a613da87c4e2daaf
Tharun Madishetti <91481732+TharunMadishetti@users.noreply.github.com>
2022-09-05 22:10:03 +0530

    Update solution.java

--- Smallest sum contiguous subarray/solution.java
+++ Smallest sum contiguous subarray/solution.java
@@ -1,20 +1,20 @@
 static int smallestSumSubarray(int a[], int size)
     {
         int cur=0,min=Integer.MAX_VALUE,pmin=a[0],p=0;
-        for(int i=0;i<a.length;i++)
+        for(int i=0;i<a.length;i++) //apply kadane's algo for minimum contiguos subarray sum 
         {
             cur+=a[i];
             if(cur>0)
             cur=0;
             if(cur<min)
             min=cur;
-            if(pmin>a[i])
+            if(pmin>a[i])       //maintain the minimum element in the whole array
             pmin=a[i];
-            if(a[i]>=0)
+            if(a[i]>=0)     // count positives
             p++;
         }
         // your code here
-if(p==a.length)
+if(p==a.length)     // if all are +ve elements then return the minimum element
 return pmin;
-return min;
+return min;     // else return minimum contiguous array found
     }
