20f967f084d22ba02e34240da0c84ab8332bdd4c
Tharun Madishetti <91481732+TharunMadishetti@users.noreply.github.com>
2022-09-11 12:29:32 +0530

    Update solution.java

--- Count occurrences of a given word in a 2-d array/solution.java
+++ Count occurrences of a given word in a 2-d array/solution.java
@@ -1,35 +1,53 @@
 public int findOccurrence(char g[][], String t)
     {
         
-        int k=0; //for returning
+        int k=0;    //for returning answer
         for(int i=0;i<g.length;i++)
         {
             for(int j=0;j<g[0].length;j++)
             {
-                k+=search(g,i,j,t,0);     // the string could start at any index of the grid
+                if(s.charAt(0)==g[i][j])
+                k+=search(g,i,j,t,0);   
+                //add all posibilities u would get by starting here
             }
         }
         return k;
     } 
-    public int search(char[][] g,int i,int j,String s,int pre)
+    //recursive method for visiting each and every possibility
+    //we need grid to check characters at positions and (i,j) acts as the present location of our
+    //character to be matched with present character in target string
+    //pre points to char to be matched with
+    public int search(char[][] g,int i,int j,String target,int pre)
     {
      
-        
-        if(i<0 || j<0 || i>g.length-1 || j>=g[0].length)  //if indices goes out of bounds
+        //out of bounds conditions
+        if(i<0 || j<0 || i>g.length-1 || j>=g[0].length)
         return 0;
-        if(g[i][j]!=s.charAt(pre))    //if present character doesn't match with character at g[i][j] then no need to proceed
+
+        //if present char at position doesn't match with present char at target string
+        if(g[i][j]!=target.charAt(pre))
         return 0;
-        if(pre==s.length()-1) found complete string till here hence no need to proceed from here
-        return 1; 
-        
-        char ch = s.charAt(pre);
-        g[i][j]='#';  //for marking this as visited
+
+        //if we reach to end of target string
+        if(pre==target.length()-1)
+        return 1;
+
+        //store present char to reassign
+        char ch = target.charAt(pre);
+
+        //marking present location as visited
+        g[i][j]='#';
+
+        //for storing answers from recursive calls
         int sum=0;
-        sum+=search(g,i,j+1,s,pre+1);
-        sum+=search(g, i, j-1, s, pre+1);
-        sum+=search(g, i+1, j, s, pre+1);
-        sum+=search(g, i-1, j, s, pre+1);
-        g[i][j]=ch;
+        //recursive calls for moving right,left,bottom and up
+        sum+=search(g,i,j+1,target,pre+1);
+        sum+=search(g, i, j-1, target, pre+1);
+        sum+=search(g, i+1, j, target, pre+1);
+        sum+=search(g, i-1, j, target, pre+1);
+        g[i][j]=ch; //reassigning the char 
+
+        //returning answer
         return sum;
 
     }
