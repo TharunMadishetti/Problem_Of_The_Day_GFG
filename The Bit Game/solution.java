6c4cfee7ce37e79b85cbd41b84dcc3bcfd8c15d6
Tharun Madishetti <91481732+TharunMadishetti@users.noreply.github.com>
2022-09-24 21:11:07 +0530

    Update solution.java

--- The Bit Game/solution.java
+++ The Bit Game/solution.java
@@ -9,9 +9,9 @@ class Solution {
         while(N!=0)
         {
             if(N%2==1)
-            c++;
+            c++;            //counting no. of set bits
             N/=2;
         }
-        return c%2==0?2:1;
+        return c%2==0?2:1;      //if even 1's exists,then game ends at 1 else ends at 2
     }
 }
