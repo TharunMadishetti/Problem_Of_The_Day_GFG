4ce43ed97a2f75c1625ed0fc316c3c2fe7891ca5
Tharun Madishetti <91481732+TharunMadishetti@users.noreply.github.com>
2022-10-04 10:04:58 +0530

    Update solution.java

--- Count all possible paths from top left to bottom right/solution.java
+++ Count all possible paths from top left to bottom right/solution.java
@@ -27,3 +27,37 @@ class Solution
     }
     
 }
+
+
+//space optimization code
+
+//User function Template for Java
+class Solution
+{
+     static int mod=(int)(1e9+7);
+    long numberOfPaths(int m, int n)
+    {
+       
+       int[] prev=new int[n+1];
+       for(int i=1;i<=m;i++)
+       {
+           int[] cur=new int[n+1];
+           for(int j=1;j<=n;j++)
+           {
+               if(i==1 && j==1)
+                cur[j]=1;
+               else if(i==1)
+               cur[j]=cur[j-1];
+               else if(j==1)
+               cur[j]=prev[j];
+               else 
+               cur[j]=(prev[j]%mod+cur[j-1]%mod)%mod;
+               
+               cur[j]%=mod;
+           }
+           prev=cur;
+       }
+       return prev[n]%mod;
+    }
+    
+}
